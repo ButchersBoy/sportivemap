d953bbc7a09e0610f33bff0dee345d11dbfc1569
James Willock <james@dragablz.net>
2016-02-19 11:09:13 +0000

    start parsing UK cycling events

--- scraperjs/index.js
+++ scraperjs/index.js
@@ -14,10 +14,12 @@ var rl = readline.createInterface({
 
 var jquery = fs.readFileSync("./bin/jquery.js", "utf-8");
 
+
+/*
 var requestOptions = {
     url: 'https://www.britishcycling.org.uk/events?search_type=upcomingevents&zuv_bc_event_filter_id[]=5&resultsperpage=1000', 
     method: "GET",     
-    proxy: 'http://917753:AliceW_1@ncproxy1:8080',
+    //proxy: 'http://917753:AliceW_1@ncproxy1:8080',
     json: false
 };
 
@@ -32,7 +34,7 @@ request(requestOptions, function (error, response, body) {
 					console.log(err)
 				else
 				{
-					parseBritishCycling(window, 'https://www.britishcycling.org.uk/events');					
+					parseBritishCycling(window, 'https://www.britishcycling.org.uk');					
 				}
 			}
 		});   					
@@ -40,36 +42,111 @@ request(requestOptions, function (error, response, body) {
 	else if (error) {
 		console.log(error);
 	}
-	rl.close();  
-	
+	rl.close();  	
 });
+*/
+
+
+var requestOptions = {
+    url: 'http://www.ukcyclingevents.co.uk/events/category/road', 
+    method: "GET",     
+    //proxy: 'http://917753:AliceW_1@ncproxy1:8080',
+    json: false
+};
+
+request(requestOptions, function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+
+		jsdom.env({
+			html : body,
+			src : [jquery],			
+			done : function (err, window) {
+				if (err)
+					console.log(err)
+				else
+				{
+					parseUkCyclingEvents(window, 'http://www.ukcyclingevents.co.uk');					
+				}
+			}
+		});   					
+	}
+	else if (error) {
+		console.log(error);
+	}
+	rl.close();  	
+});
+
+
+
 
 rl.question("Enter to exit > ", function(answer) {
   console.log("End:", answer);
   rl.close();
 });
 
+function Event(name, date, kind, locationSummary, indexerUrl) {
+	this.name = name;
+	this.date = date;
+	this.kind = kind;
+	this.locationSummary = locationSummary;
+	this.indexerUrl = indexerUrl;
+}
+Event.prototype.toJSON = function() {
+	return JSON.stringify({
+		name : this.name,
+		date : this.date,
+		kind : this.kind,
+		locationSummary : this.locationSummary,
+		indexerUrl : this.indexerUrl
+	});
+}
+
+
+
+function readJSON(filename){
+  return new Promise(function (fulfill, reject){
+    readFile(filename, 'utf8').done(function (res){
+      try {
+        fulfill(JSON.parse(res));
+      } catch (ex) {
+        reject(ex);
+      }
+    }, reject);
+  });
+}
+
+
+function parseUkCyclingEvents(window, indexerRootUrl) {
+	var pages = window.$("ul.thumbnails:first a").map(function(index, a) {
+		return (indexerRootUrl + a.href);
+	});
+	console.log(pages[0]);
+}
+
 function parseBritishCycling(window, indexerUrlPrefix)
-{
-	
-	var x = window.$("tr.events--desktop__row:has(td)").map(function(index, tr) {		
-		var event = {
-			name : window.$("td.table__more-cell", tr).text(),
-			date : window.$("td.event--date__column", tr).text(),			
-			kind : window.$("td.event--type__row", tr).text(),						 
-			locationSummary : window.$("td:not([class]):last", tr).text(),
-			indexerUrl : indexerUrlPrefix.concat(window.$("td.events--event__column > a", tr).attr('href'))
-		}; 
+{	
+	var events = window.$("tr.events--desktop__row:has(td)").map(function(index, tr) {				
+		var event = new Event(
+			window.$("td.table__more-cell", tr).text().clean(),
+			window.$("td.event--date__column", tr).text().clean(),			
+			window.$("td.event--type__row", tr).text().clean(),						 
+			window.$("td:not([class]):last", tr).text().clean(),
+			indexerUrlPrefix.concat(window.$("td.events--event__column > a", tr).attr('href')).clean()
+		); 
 		return event; 		
 	})
 	
-	console.log("Item count: " + x.length);
+	console.log("Item count: " + events.length);
 	
-	console.log(x[5].name);
-	console.log(x[5].date);
-	console.log(x[5].kind);
-	console.log(x[5].locationSummary);
-	console.log(x[5].indexerUrl);
+	console.log(events[5].name);
+	console.log(events[5].date);
+	console.log(events[5].kind);
+	console.log(events[5].locationSummary);
+	console.log(events[5].indexerUrl);
 	
-	//console.log("contents of a.the-link:", window.$("tr.events--desktop__row:has(td)").text());
+	console.log(events[5].toJSON());
+}
+
+String.prototype.clean = function() {
+	return this.trim().replace('/r', '/');
 }
