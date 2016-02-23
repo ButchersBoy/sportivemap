91b3a5eba7bf72c407f8fced79202d9f0971d5a1
James Willock <james@dragablz.net>
2016-02-23 08:37:54 +0000

    sort data

---
+++ scraperjs/sort.js
@@ -0,0 +1,19 @@
+
+var fs = require("fs");
+
+var text = fs.readFileSync("data-uk.js",  'utf8');
+var events = JSON.parse(text);
+console.log(events.length);
+console.log(events[0]);
+
+events.sort(function(a, b) {
+    return Date.parse(b.date).valueOf() - Date.parse(a.date).valueOf();
+});
+
+console.log("sorted.");
+
+for (var i = events.length - 1; i >= 0; i--) {
+	console.log(events[  i].date);
+};
+
+fs.writeFileSync('data-uk-sorted.js', JSON.stringify(events), 'utf8');
