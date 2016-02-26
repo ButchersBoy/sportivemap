395c703a063791a8f0949d30491c784c85fa13b6
ButchersBoy <james.willock@mulhollandsoftware.com>
2016-02-26 13:08:19 +0000

    missing events

---
+++ scraperjs/3-sort-insert-trim.js
@@ -0,0 +1,33 @@
+
+var fs = require("fs");
+
+
+var text = fs.readFileSync("3-additional-input.json",  'utf8');
+var additionalData = JSON.parse(text);
+console.log("Manual entries to add: " + additionalData.addEntries.length);
+console.log("Manual entries to remove: " + additionalData.removeEntries.length);
+
+
+text = fs.readFileSync("data-uk.json",  'utf8');
+var events = JSON.parse(text);
+console.log("Loaded existing: " + events.length);
+//console.log(events[0]);
+
+additionalData.addEntries.forEach((ev) => {
+	console.log("Adding manual entry " + ev.name);
+	events.push(ev);	
+}, this);
+
+events.sort(function(a, b) {
+    return Date.parse(a.date).valueOf() - Date.parse(b.date).valueOf();
+});
+
+console.log("Sorted: " + events.length);
+
+/*
+for (var i = events.length - 1; i >= 0; i--) {
+	console.log(events[i].date);
+};
+*/
+
+fs.writeFileSync('data-uk-sorted.json', JSON.stringify(events), 'utf8');
