ab368c0a054b83f314212933795704623cb58408
James Willock <james@dragablz.net>
2016-02-29 21:47:57 +0000

    sell my soul

--- website/bin/main.js
+++ website/bin/main.js
@@ -15,10 +15,17 @@ class SportiveInfoWindow extends React.Component {
     render() {
         return (
             <div className={"ui"}>
-                <div className={"header"}>{this.props.item.name}</div>
-                <div>{Moment(this.props.item.date).format("dddd, MMMM Do YYYY")}</div>
-                <div>{this.props.item.formattedAddress}</div>               
-                <div><a href={this.props.item.indexerUrl} target="_blank">Website</a></div>
+                <div className={"info-detail"}>
+                    <div className={"header"}>{this.props.item.name}</div>
+                    <div>{Moment(this.props.item.date).format("dddd, MMMM Do YYYY")}</div>
+                    <div>{this.props.item.formattedAddress}</div>               
+                    <div><a href={this.props.item.indexerUrl} target="_blank">Website</a></div>
+                </div>
+                <div className={"info-advert"}>
+                    <a href="https://www.awin1.com/cread.php?s=228086&v=1857&q=113306&r=267983">
+                        <img src="https://www.awin1.com/cshow.php?s=228086&v=1857&q=113306&r=267983" border="0" />
+                    </a>
+                </div>                                                                
             </div>);
     }
 }
